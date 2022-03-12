9168b1b5e0a3467dd03eba8e348d806ba16f2c67
John Akhilomen <jonbasanti@gmail.com>
2022-03-11 18:48:11 -0600

    msg

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Start from './components/Start';
+import React from "react";
+import Start from "./src/Start";
+// import Chat from "./src/Chat";
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { TextInput, TouchableOpacity } from "react-native-gesture-handler";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-     <Start />
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    const Stack = createStackNavigator();
+    return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Start">
+          <Stack.Screen
+            name="Start"
+            component={Start}
+            options={{ title: "Welcome to Chatter!" }}
+          />
+          {/* <Stack.Screen name="Chat" component={Chat} /> */}
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/Start.js
@@ -0,0 +1,159 @@
+import React, { Component } from 'react';
+import { ImageBackground, StyleSheet, View, Text, TextInput, Button, Alert } from 'react-native';
+import bgImage from "../assets/images/bg.jpeg";
+
+export default class Start extends React.Component {
+ constructor(props) {
+   super(props);
+   this.state = { text: '' };
+ }
+
+ alertMyText (input) {
+  Alert.alert(input.text);
+}
+
+colors = {
+  orange: "#f44336",
+  magenta: "#e91e63",
+  fucsia: "#9c27b0",
+  purple: "#673ab7",
+};
+
+ render() {
+   return (
+    <View >
+    <ImageBackground
+     source={bgImage}
+      resizeMode="cover"
+      style={styles.bgImage}
+    >
+      <Text style={styles.h1}>Texter</Text>
+      <View style={styles.box}>
+        <TextInput
+          style={styles.input}
+          onChangeText={(text) => this.setState({ text: text })}
+          value={this.state.name}
+          placeholder="Your Name"
+        />
+        <View style={styles.colorSwatch}>
+          <Text style={styles.subtitle}>Choose Background Color</Text>
+          <View style={styles.swatches}>
+              <View style={styles.swatch1}></View>
+              <View style={styles.swatch2}></View>
+              <View style={styles.swatch3}></View>
+              <View style={styles.swatch4}></View>
+          </View>
+        </View>
+        <Button
+          style={styles.btn}
+          title="Start Chatting"
+          color={"#6705e9"}
+          containerViewStyle={{ width: "100%", marginLeft: 0 }}
+          onPress={() =>
+            this.alertMyText({text: this.state.text})
+          }
+        />
+      </View>
+    </ImageBackground>
+  </View>
+     
+   );
+   
+ }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    backgroundColor: "#151617",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  bgImage: {
+     resizeMode: 'cover', 
+     width: '100%', 
+     height: '100%',
+     alignItems: 'center',
+  },
+  h1: {
+    flexGrow: 1,
+    flexShrink: 1,
+    fontWeight: "800",
+    color: "transparent",
+    paddingTop: 60,
+  },
+  box: {
+    backgroundColor: "#ffffffc4",
+    flexGrow: 1,
+    flexShrink: 0,
+    width: "88%",
+    marginBottom: 40,
+    flexDirection: "column",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    padding: 30,
+    height: 260,
+    minHeight: 260,
+    maxHeight: 290,
+    borderRadius: 20,
+  },
+  input: {
+    flex: 1,
+    height: 50,
+    maxHeight: 50,
+    borderColor: "gray",
+    borderWidth: 1,
+    width: "88%",
+    padding: 5,
+    paddingLeft: 10,
+    fontSize: 16,
+    fontWeight: "300",
+    color: "#757083",
+    opacity: 0.5,
+  },
+  btn: {
+    flex: 1,
+  },
+  subtitle: {
+    fontSize: 16,
+    fontWeight: "300",
+    color: "#757083",
+    opacity: 1,
+    marginBottom: 10,
+  },
+  colorSwatch: {
+    flex: 1,
+    padding: 20,
+    marginTop: 5,
+  },
+  swatches: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  swatch1: {
+    width: 40,
+    height: 40,
+    backgroundColor: "#f44336",
+    borderRadius: 40,
+  },
+  swatch2: {
+    width: 40,
+    height: 40,
+    backgroundColor: "#e91e63",
+    borderRadius: 40,
+  },
+  swatch3: {
+    width: 40,
+    height: 40,
+    backgroundColor: "#9c27b0",
+    borderRadius: 40,
+  },
+  swatch4: {
+    width: 40,
+    height: 40,
+    backgroundColor: "#673ab7",
+    borderRadius: 40,
+  },
+});
